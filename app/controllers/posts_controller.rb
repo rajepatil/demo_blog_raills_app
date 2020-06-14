511a86635aeb49a6165c5083629f71d1c7154dc3
Rajesh Mule <rajeshsmule@gmail.com>
2020-06-14 20:50:31 +0530

    finish the curd of comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+  
   def index 
     @posts = Post.all.order("created_at DESC")
   end
@@ -19,11 +20,11 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    
   end
 
   def update
-    @post = Post.find(params[:id])
+   
 
     if @post.update(post_params)
       redirect_to @post
@@ -33,12 +34,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    
 
   end
 
   def destroy
-    @post = Post.find(params[:id])
+   
     @post.destroy
 
     redirect_to posts_path
